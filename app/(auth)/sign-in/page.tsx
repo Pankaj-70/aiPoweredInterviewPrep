49a13fdba2be4f7538b169b239d5cec270d48ce5
Pankaj Sharma <146180500+Pankaj-70@users.noreply.github.com>
2025-10-31 04:36:24 +0000

    restarting project

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/components/AuthForm';
-import React from 'react'
-
-const SignInPage = () => {
-  return <AuthForm type="sign-in"></AuthForm>
-
-}
-
-export default SignInPage;
--- app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import AuthForm from '@/components/AuthForm';
-
-const SignUpPage = () => {
-  
-  return <AuthForm type="sign-up"></AuthForm>
-}
-
-export default SignUpPage;
